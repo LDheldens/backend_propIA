dd2aa8342570b14b6985d3320260bef637dba1df
kolynhj <112416424+KLN1915@users.noreply.github.com>
2024-07-03 10:23:45 -0500

    contact response updated

--- contact/views.py
+++ contact/views.py
@@ -18,7 +18,7 @@ def create_message(request):
         serializer = MessageSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({'message': 'Se registró el mensaje correctamente'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
